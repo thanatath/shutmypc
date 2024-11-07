9e488347535bb17c8901c5a426a0d0aaadffa4ce
thanatach <thanatach.as@gmail.com>
2024-11-08 02:06:45 +0700

    initial

---
+++ Service.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace shutmypc
+{
+    public partial class Service : ServiceBase
+    {
+        private IHost _host;
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            _host.StartAsync();
+        }
+
+        protected override void OnStop()
+        {
+            _host?.StopAsync().Wait();
+            _host?.Dispose();
+        }
+        public void RunAsConsole()
+        {
+            OnStart(null);
+            Console.WriteLine("Service running... Press Enter to stop.");
+            Console.ReadLine();
+            OnStop();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace shutmypc
+{
+    public class Worker : BackgroundService
+    {
+        private HttpListener _listener;
+        private const string url = "http://localhost:5000/hibernation/";
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(url);
+            _listener.Start();
+
+            Console.WriteLine("Service started and listening for HTTP hook...");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var context = await _listener.GetContextAsync();
+                if (context.Request.HttpMethod == "POST")
+                {
+                    HibernatePC();
+                    Console.WriteLine("Received hook, hibernating PC.");
+                }
+
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.Close();
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _listener.Stop();
+            await base.StopAsync(stoppingToken);
+        }
+
+        private void HibernatePC()
+        {
+            // Use Windows API to hibernate the PC
+            SetSuspendState(true, true, true);
+        }
+
+        [DllImport("powrprof.dll", SetLastError = true)]
+        private static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+    }
+}
